eae1162b443a36ac6d6d7b82ad27a16d1ee3d6c6
Tony Kovanen <tonykovanen@hotmail.com>
2013-10-15 21:02:58 +0300

    Removed Boost includes and added a better description to README

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,10 +8,9 @@ OPENCV_CAMERA_MODULES:=on
 
 LOCAL_MODULE    := native_activity
 LOCAL_C_INCLUDES:= /home/tonykova/workspace/OpenCV-2.4.6-android-sdk/sdk/native/jni/include
-LOCAL_C_INCLUDES+= /usr/local/boost_1_54_0
 LOCAL_SRC_FILES := native.cpp nonfree_init.cpp precomp.cpp sift.cpp surf.cpp
 LOCAL_LDLIBS    += -lm -llog -landroid
-LOCAL_STATIC_LIBRARIES := android_native_app_glue boost_thread-gcc-mt-s-1_46
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_SHARED_LIBRARIES := sift_prebuilt opencv_java_prebuilt
 include $(BUILD_SHARED_LIBRARY)
 
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,4 @@
 APP_ABI := armeabi-v7a
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions -std=c++11 -pthread
-APP_CPPFLAGS += /usr/local/boost_1_54_0/libs/
 APP_PLATFORM := android-15
