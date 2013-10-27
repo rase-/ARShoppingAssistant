1d0b258b52c707278a451c23e38c394d5a608fa6
Tony Kovanen <tonykovanen@hotmail.com>
2013-10-27 22:37:04 +0200

    Now successfully linked tess-two

--- jni/Android.mk
+++ jni/Android.mk
@@ -12,38 +12,10 @@ OPENCV_CAMERA_MODULES:=on
 LOCAL_STATIC_LIBRARIES += ../zxing/build/libzxing.a
 LOCAL_C_INCLUDES += /home/tonykova/workspace/zxing/build/core/src
 
-# Include tesseract, try1
-#include ../tess-two/tess-two/jni/Android.mk
-# Fix path
-#LOCAL_PATH := $(call my-dir)
-# Trying to include tesseract as a static lib, try2
-#TESSERACT_PATH := ../tess-two/tess-two/com_googlecode_tesseract_android/src
-#LEPTONICA_PATH := ../tess-two/tess-two/com_googlecode_leptonica_android/src
-#LOCAL_STATIC_LIBRARIES += ../tess-two/tess-two/libs/armeabi/liblept.so
-#LOCAL_STATIC_LIBRARIES += ../tess-two/tess-two/libs/armeabi/libtess.so
-#LOCAL_C_INCLUDES += \
-#  $(TESSERACT_PATH)/api \
-#  $(TESSERACT_PATH)/ccmain \
-#  $(TESSERACT_PATH)/ccstruct \
-#  $(TESSERACT_PATH)/ccutil \
-#  $(TESSERACT_PATH)/classify \
-#  $(TESSERACT_PATH)/cube \
-#  $(TESSERACT_PATH)/cutil \
-#  $(TESSERACT_PATH)/dict \
-#  $(TESSERACT_PATH)/image \
-#  $(TESSERACT_PATH)/neural_networks/runtime \
-#  $(TESSERACT_PATH)/textord \
-#  $(TESSERACT_PATH)/viewer \
-#  $(TESSERACT_PATH)/wordrec \
-#  $(LEPTONICA_PATH)/src
-#  Trying to include by doing what tess-two Android.mk is doing, try3
-TESSERACT_PATH := ../tess-two/tess-two/jni/com_googlecode_tesseract_android/src
-LEPTONICA_PATH := ../tess-two/tess-two/jni/com_googlecode_leptonica_android/src
-include ../tess-two/tess-two/jni/com_googlecode_leptonica_android/Android.mk
-include ../tess-two/tess-two/jni/com_googlecode_tesseract_android/Android.mk
+# Include tesseract
+LOCAL_SHARED_LIBRARIES += ../tess-two/tess-two/libs/armeabi-v7a/liblept.so
+LOCAL_SHARED_LIBRARIES += ../tess-two/tess-two/libs/armeabi-v7a/libtess.so
 
-# Just build the Android.mk files in the subdirs
-# include $(call all-subdir-makefiles)
 LOCAL_MODULE    := native_activity
 # Include OpenCV native jni include dir
 LOCAL_C_INCLUDES+= /home/tonykova/workspace/OpenCV-2.4.6-android-sdk/sdk/native/jni/include
@@ -51,7 +23,7 @@ LOCAL_C_INCLUDES+= /home/tonykova/workspace/OpenCV-2.4.6-android-sdk/sdk/native/
 LOCAL_SRC_FILES := native.cpp nonfree_init.cpp precomp.cpp sift.cpp surf.cpp
 LOCAL_LDLIBS    += -lm -llog -landroid
 LOCAL_STATIC_LIBRARIES += android_native_app_glue
-LOCAL_SHARED_LIBRARIES := sift_prebuilt opencv_java_prebuilt
+LOCAL_SHARED_LIBRARIES += sift_prebuilt opencv_java_prebuilt
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
